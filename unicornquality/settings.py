7e73e34f0415c34003e0be759b9b69785d0cb0c5
mickh14 <michael.hurley@expleogroup.com>
2020-03-15 17:19:04 +0000

    add allowed host to settings

--- unicornquality/settings.py
+++ unicornquality/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'unicorn-quality.herokuapp.com']
 
 
 # Application definition
@@ -82,12 +82,7 @@ WSGI_APPLICATION = 'unicornquality.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
- #       'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   # }
-#}
+
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 else:
