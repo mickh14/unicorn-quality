0835bba1712da7527e828da1b5aaa1d163641462
mickh14 <michael.hurley@expleogroup.com>
2020-03-15 17:54:35 +0000

    update settings

--- unicornquality/settings.py
+++ unicornquality/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
 import dj_database_url
+
+try:
+    import env
+except ImportError:
+    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
