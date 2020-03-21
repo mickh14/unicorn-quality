652ae4d6ef05e7ce4411a54eba1dc083bdfd0b60
mickh14 <michael.hurley@expleogroup.com>
2020-03-21 18:06:43 +0000

    add style to all pages

--- issues/forms.py
+++ issues/forms.py
@@ -5,7 +5,7 @@ from .models import Issue, Comment
 class IssueForm(forms.ModelForm):
     class Meta:
         model = Issue
-        fields = ('name', 'description', 'status', 'step_to_Reproduce', 'expected_Results', 'actual_Results', 'issue_type', 'published_date')
+        fields = ('name', 'description', 'status', 'step_to_Reproduce', 'expected_Results', 'actual_Results', 'issue_type')
 
 
 class CommentForm(forms.ModelForm):
